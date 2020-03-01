08f5cbc2985fc93c5f9eec3755b90f9ba12ab0f2
George Travis <wewerelikesharks@gmail.com>
2020-02-29 21:04:27 -0800

    sprites loaded

--- Scene2.js
+++ Scene2.js
@@ -6,7 +6,31 @@ class Scene2 extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(20, 20, 'Playing Game (yellow)', {
+    this.background = this.add.image(0, 0, 'background');
+    this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.image(
+      config.width / 2 - 50,
+      config.height / 2,
+      'ship'
+    );
+    this.ship2 = this.add.image(config.width / 2, config.height / 2, 'ship2');
+    this.ship3 = this.add.image(
+      config.width / 2 + 50,
+      config.height / 2,
+      'ship3'
+    );
+
+    const ship2 = this.ship2;
+    const ship3 = this.ship3;
+
+    this.ship1.setScale(2).flipY = true;
+    let interval = setInterval(function() {
+      ship2.angle += 3;
+      ship3.angle -= 3;
+    }, 25);
+
+    this.add.text(20, 20, 'Playing game', {
       font: '25px Arial',
       fill: 'yellow'
     });
--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const config = {
-  width: 800,
-  height: 600,
+  width: 256,
+  height: 272,
   backgroundColor: 0x000000,
   scene: [Scene1, Scene2]
 };
