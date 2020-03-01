08dde96cdc945758dfad2ce6e6b9cd1484f83235
George Travis <wewerelikesharks@gmail.com>
2020-03-01 14:03:37 -0800

    introduced handlebars

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 const PORT = process.env.PORT || 3000;
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const routes = require('./controllers/routes.js');
 const app = express();
-app.use(express.static(__dirname + '/'));
-app.get('/', (req, res) => {
-  res.send('index.html');
-});
+app.use(express.static('public'));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(routes);
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
 app.listen(PORT, function() {
   console.log('Server running at: http://localhost:' + PORT);
 });
