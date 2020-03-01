4e902f4c93a95ccc1c8182b306943eccf221e643
George Travis <wewerelikesharks@gmail.com>
2020-02-29 20:24:49 -0800

    initial commit

---
+++ Scene2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Scene2 extends Phaser.Scene {
+  constructor() {
+    super('playGame');
+  }
+
+  create() {
+    this.add.text(20, 20, 'Playing game', {
+      font: '25px Arial',
+      fill: 'yellow'
+    });
+  }
+}
---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(PORT, function() {
+  console.log('Server running at: http://localhost:' + PORT);
+});
