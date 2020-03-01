be62dfcddfbc9c263a7dbf8b1e9df6bddf525a91
George Travis <wewerelikesharks@gmail.com>
2020-02-29 20:28:37 -0800

    updated server .get

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/'));
+app.get('/', (req, res) => {
+  res.send('index.html');
+});
 app.listen(PORT, function() {
   console.log('Server running at: http://localhost:' + PORT);
 });
