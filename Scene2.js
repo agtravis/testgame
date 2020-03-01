fb5649cafb3087c52dc99c7c5bde3306bc89a3f3
George Travis <wewerelikesharks@gmail.com>
2020-03-01 13:18:52 -0800

    ship destroys, reload page restarts app

--- Scene2.js
+++ Scene2.js
@@ -19,17 +19,64 @@ class Scene2 extends Phaser.Scene {
       font: '25px Arial',
       fill: 'yellow'
     });
-    this.ship1 = this.add.image(
+    // this.ship1 = this.add.image(
+    //   config.width / 2 - 50,
+    //   config.height / 2,
+    //   'ship'
+    // );
+    // this.ship2 = this.add.image(config.width / 2, config.height / 2, 'ship2');
+    // this.ship3 = this.add.image(
+    //   config.width / 2 + 50,
+    //   config.height / 2,
+    //   'ship3'
+    // );
+    this.ship1 = this.add.sprite(
       config.width / 2 - 50,
       config.height / 2,
       'ship'
     );
-    this.ship2 = this.add.image(config.width / 2, config.height / 2, 'ship2');
-    this.ship3 = this.add.image(
+    this.ship2 = this.add.sprite(config.width / 2, config.height / 2, 'ship2');
+    this.ship3 = this.add.sprite(
       config.width / 2 + 50,
       config.height / 2,
       'ship3'
     );
+
+    this.anims.create({
+      key: 'ship1_anim',
+      frames: this.anims.generateFrameNumbers('ship'),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'ship2_anim',
+      frames: this.anims.generateFrameNumbers('ship2'),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'ship3_anim',
+      frames: this.anims.generateFrameNumbers('ship3'),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'explode',
+      frames: this.anims.generateFrameNumbers('explosion'),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+
+    this.ship1.play('ship1_anim');
+    this.ship2.play('ship2_anim');
+    this.ship3.play('ship3_anim');
+
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+
+    this.input.on('gameobjectdown', this.destroyShip, this);
   }
 
   moveShip(ship, speed) {
@@ -43,6 +90,11 @@ class Scene2 extends Phaser.Scene {
     ship.y = 0;
     const randomX = Phaser.Math.Between(0, config.width);
     ship.x = randomX;
+  }
+
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture('explosion');
+    gameObject.play('explode');
   }
 
   update() {
