a359535ddee2e75cd3d0c34b142f97ec3d241b34
George Travis <wewerelikesharks@gmail.com>
2020-03-03 23:54:29 -0800

    commented out routers

--- controllers/routes.js
+++ controllers/routes.js
@@ -8,32 +8,32 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/phaser', (req, res) =>
-  res.sendFile(path.join(__dirname, '../external/phaser.min.js'))
-);
+// router.get('/phaser', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../external/phaser.min.js'))
+// );
 
-router.get('/scene1', (req, res) =>
-  res.sendFile(path.join(__dirname, '../Scene1.js'))
-);
+// router.get('/scene1', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../Scene1.js'))
+// );
 
-router.get('/scene2', (req, res) =>
-  res.sendFile(path.join(__dirname, '../Scene2.js'))
-);
+// router.get('/scene2', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../Scene2.js'))
+// );
 
-router.get('/scene3', (req, res) =>
-  res.sendFile(path.join(__dirname, '../Scene3.js'))
-);
+// router.get('/scene3', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../Scene3.js'))
+// );
 
-router.get('/beam', (req, res) =>
-  res.sendFile(path.join(__dirname, '../beam.js'))
-);
+// router.get('/beam', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../beam.js'))
+// );
 
-router.get('/explosion', (req, res) =>
-  res.sendFile(path.join(__dirname, '../explosion.js'))
-);
+// router.get('/explosion', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../explosion.js'))
+// );
 
-router.get('/game', (req, res) =>
-  res.sendFile(path.join(__dirname, '../game.js'))
-);
+// router.get('/game', (req, res) =>
+//   res.sendFile(path.join(__dirname, '../game.js'))
+// );
 
 module.exports = router;
